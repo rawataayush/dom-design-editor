12e6a36cc65c97e23e14dab05da96963feb78a24
Aayush Rawat <aayushrawat.dev@gmail.com>
2026-01-23 20:39:30 +0530

    feat: add keyboard delete support and clean up app entry

--- js/app.js
+++ js/app.js
@@ -1,25 +1,42 @@
 import { createRectangle, createText } from "./elements/create.js";
 import { selectElementById, clearElementSelection } from "./elements/select.js";
+
 import "./elements/drag.js";
 import "./elements/resize.js";
+
 import { isInteractionActive } from "./state/interaction.js";
-import { renderLayers } from "./panels/layer.js";
+import { 
+    getSelectedElementId,
+    clearSelection,
+    removeElement, 
+    loadFromLocalStorage, 
+    saveToLocalStorage
+} from "./state/store.js";
+
 import { renderCanvas } from "./canvas/render.js";
-import { loadFromLocalStorage, saveToLocalStorage } from "./state/store.js";
+import { renderLayers } from "./panels/layer.js";
+
 import { exportJSON } from "./utils/export.js";
 import { exportHTML } from "./utils/exportHtml.js";
 
-document.getElementById("add-rect-btn").addEventListener('click', createRectangle);
-document.getElementById('add-text-btn').addEventListener('click', createText);
-
-// Temporarily disabled
-// document.getElementById('add-circle-btn').addEventListener('click', createCircle);
+// DOM REFERENCES
+const canvas = document.getElementById('canvas');
+const rectBtn = document.getElementById("add-rect-btn")
+const textBtn = document.getElementById('add-text-btn');
 const circleBtn = document.getElementById("add-circle-btn");
+const exportJsonBtn = document.getElementById("export-json-btn");
+const exportHtmlBtn = document.getElementById("export-html-btn");
 
+
+// TOOLBAR ACTIONS
+rectBtn.addEventListener('click', createRectangle);
+textBtn.addEventListener('click', createText);
+
+// Circle intenionally disabled
 circleBtn.disabled = true;
 circleBtn.title = "Coming soon";
 
-const canvas = document.getElementById('canvas');
+// CANVAS SELECTION 
 canvas.addEventListener('mousedown', (e)=> {
     if (isInteractionActive()) return;
     
@@ -30,8 +47,26 @@ canvas.addEventListener('mousedown', (e)=> {
     }
 });
 
-document.getElementById("export-json-btn").addEventListener("click", exportJSON);
-document.getElementById("export-html-btn").addEventListener("click", exportHTML);
+// EXPORT ACTIONS
+exportJsonBtn.addEventListener("click", exportJSON);
+exportHtmlBtn.addEventListener("click", exportHTML);
+
+// KEYWORD CONTROLS
+
+document.addEventListener("keydown", (e) => {
+    const selectedId = getSelectedElementId();
+    if(!selectedId) return;
+
+    if(e.key === "Delete" || e.key === "Backspace") {
+        removeElement(selectedId);
+        clearSelection();
+        saveToLocalStorage();
+        renderCanvas();
+        renderLayers();
+    }
+})
+
+// INITIAL LOAD
 
 function renderAll () {
     loadFromLocalStorage();
